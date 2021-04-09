f0394fe6d6ad14e6cfa96ad9eaa8f3f0f52bf61e
Roko <64219362+Roko191@users.noreply.github.com>
2021-04-09 10:31:27 +0200

    Delete netconf.bat

--- netconf.bat
+++
@@ -1,9 +0,0 @@
-@ECHO OFF
-color 0a
-ECHO Chenking your IP...
-ipconfig | findstr IPv4
-ECHO Press any key if you want to see your whole Network config
-PAUSE
-ipconfig
-ECHO Press any key to exit
-PAUSE
